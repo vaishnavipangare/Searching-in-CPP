eb94cbafacca6f010221648307a565617e25037e
Vaishnavi Pangare <vaishnavi.pangare.btech2024@sitpune.edu.in>
2025-09-26 20:32:15 -1200

    Update Linear Search.cpp

--- Linear Search.cpp
+++ Linear Search.cpp
@@ -1,5 +1,5 @@
-// Aditya Bhagwan Jadhav
-// 24070123005
+// Vaishnavi Pangare
+// 25070123501
 
 #include <iostream>
 using namespace std;
@@ -26,4 +26,5 @@ int main() {
 }
 
 // OUTPUT
+
 // Element found at index 2
