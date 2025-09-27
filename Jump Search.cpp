775e1f044ddb2838e4c0f61a4375cacf0be68540
Vaishnavi Pangare <vaishnavi.pangare.btech2024@sitpune.edu.in>
2025-09-26 20:32:51 -1200

    Update Jump Search.cpp

--- Jump Search.cpp
+++ Jump Search.cpp
@@ -1,5 +1,5 @@
-// Aditya Bhagwan Jadhav
-// 24070123005
+// Vaishnavi Pangare
+// 25070123501
 
 #include <iostream>
 #include <cmath>
@@ -36,4 +36,5 @@ int main() {
 }
 
 // OUTPUT
+
 // Element found at index 4
